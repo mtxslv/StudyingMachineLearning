2d525ee37709e45061fe10a986906cfaee32cd76
user41 <user41@ectufrn.ect.ufrn.br>
2018-12-03 14:36:40 -0300

    organizando

---
+++ kmeans/kmeans.cpp
@@ -0,0 +1,13 @@
+#include "kmeans.h"
+
+Kmeans::Kmeans(){
+
+}
+
+int Kmeans::getCluster(int* features, int _dim){
+
+}
+
+Kmeans::Kmeans(int _dim, int* _clusters){ // entra com as dimensões e aos clusters
+
+}
