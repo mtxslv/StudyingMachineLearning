e1d3cfd2b42a47ee0b73f566d02e1490d45ef756
Mateus de Assis Silva <33188461+TheAssis@users.noreply.github.com>
2018-12-05 20:07:37 -0200

    Update kmeans.h

--- kmeans/kmeans.h
+++ kmeans/kmeans.h
@@ -1,19 +1,15 @@
 #ifndef KMEANS_H
 #define KMEANS_H
 
-class Kmeans
-{
-    public:
-        Kmeans(); // construtor
-        Kmeans(int _dim, int* _clusters); // entra com as dimensões e aos clusters
-        int getCluster(int * features, int _dim); // entrou com um dado, esse método diz qual o cluster a qual pertence
-
-    protected:
-
-    private:
-        int num_clusters = 0; // numero de clusters
-        int dim = 0; // dimensão de cada elemento do cluster
-        int * clusters = new int [num_clusters*dim]; // ponteiro para os clusters
+class Kmeans{
+private:
+    float * clusters;
+    int dim_clusters;
+    int qtd_clusters;
+public:
+    Kmeans(float * _clusters,int _dim, int _qtd);
+    float * get_centroid(float * features);
+    friend float metric(float *cluster, float *feat);
 };
 
 #endif // KMEANS_H
