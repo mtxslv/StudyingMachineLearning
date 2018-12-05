51cd1acb05e02840d1aa5e0b2842a2b73d998b51
Mateus de Assis Silva <33188461+TheAssis@users.noreply.github.com>
2018-12-05 20:07:04 -0200

    Update kmeans.cpp

--- kmeans/kmeans.cpp
+++ kmeans/kmeans.cpp
@@ -1,13 +1,32 @@
 #include "kmeans.h"
+#include <cmath>
 
-Kmeans::Kmeans(){
-
+Kmeans::Kmeans(float *_clusters, int _dim, int _qtd)
+{
+    clusters = _clusters;
+    dim_clusters = _dim;
+    qtd_clusters = _qtd;
+}
+float metric(float *cluster, float *feat)
+{
+    float metric = 0;
+    for(int i = 0;i < 2; i++){
+       metric += pow(*(cluster + i) - *(feat + i), 2);
+    }
+    return sqrt(metric);
 }
 
-int Kmeans::getCluster(int* features, int _dim){
-
-}
-
-Kmeans::Kmeans(int _dim, int* _clusters){ // entra com as dimensões e aos clusters
-
+float *Kmeans::get_centroid(float *features)
+{
+    float menor_medida = metric(clusters, features);
+    float medida;
+    float * cls = clusters;
+    for(int i = 1; i < qtd_clusters; i++){
+        medida = metric(clusters + i*dim_clusters, features);
+        if(medida < menor_medida){
+            cls = clusters + i*dim_clusters;
+            menor_medida = medida;
+        }
+    }
+    return cls;
 }
