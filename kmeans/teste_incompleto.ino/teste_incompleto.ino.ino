d8936a2cf28089615ff0a5084bc93bbb1fe10048
Mateus de Assis <mateusassis@ufrn.edu.br>
2018-12-09 18:29:10 -0300

    começando a trabalhar na parte do Arduino

---
+++ kmeans/teste_incompleto.ino/teste_incompleto.ino.ino
@@ -0,0 +1,16 @@
+#include <kmeans.h>
+
+float centroides[9] = {122.5,76.167, 156.667,    66.222, 273.667, 153.944,       22.222, 48.278, 39.056};
+int dim = 3, qtd = 3;
+Kmeans my_kmns(centroides,dim,qtd);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  
+  // put your main code here, to run repeatedly:
+
+}
